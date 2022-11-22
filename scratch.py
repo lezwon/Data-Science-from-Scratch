062e6dd2e0681b279c6b7445639367818878f537
lezwon.castellino@swymcorp.com <lezwon.castellino@swymcorp.com>
2022-11-22 10:03:46 +0000

    add new commits

---
+++ scratch.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC This is a test file to check version control in databricks
+
+# COMMAND ----------
+
+print("Hello darkness my old friend")
+
+# COMMAND ----------
+
+import platform
+platform.machine()
+platform.version()
+platform.platform()
+platform.uname()
+platform.system()
+platform.processor()
